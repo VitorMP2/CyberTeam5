738287079ffc1918cf921aa52c355ca55793d38e
Lexa0311 <90433287+Lexa0311@users.noreply.github.com>
2021-09-20 09:30:48 -0500

    Creacion entidad Personas

---
+++ Personas.cs
@@ -0,0 +1,13 @@
+using System;
+namespace UCP.App.Dominio
+{
+	public class Personas
+	{
+		public int id;
+		public string nombre{get;set;};
+		public string apellido{get;set;}
+		public int identificacion{get;set;}
+		public int edad{get;set;}
+		public string estado{get;set;}
+	}
+}
