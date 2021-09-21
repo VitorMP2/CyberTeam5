276c220ed62b1c4656d2be5144961bf696a5eb6d
KateHurtado <90659145+KateHurtado@users.noreply.github.com>
2021-09-21 18:13:16 -0500

    Create Personal_cocina.cs

---
+++ CTRestaurant.App/CTRestaurant.App.Dominio/Entidades/Personal_cocina.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CTRestaurant.App.Dominio
+{
+    public class Personal_cocina
+    {
+        public int Id{get;set;}
+        public int Turno_servcio{get;set;}
+   
+
+    }
+}
