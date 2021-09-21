ad6c8cb23e9847c309fad9f9a7a2f2898cfdbf21
MiguelAngel1996 <90433460+MiguelAngel1996@users.noreply.github.com>
2021-09-21 11:52:33 -0500

    Update Turno.cs
    Se agrega Id a entidad Turno

--- Turno.cs
+++ Turno.cs
@@ -4,6 +4,7 @@ namespace UCR.App.Dominio
 {
     public class Turno
     {
+        public int Id{get;set;}
         public string Menu{get;set;}
         public DateTime HorarioAsistencia{get;set;}
     }
