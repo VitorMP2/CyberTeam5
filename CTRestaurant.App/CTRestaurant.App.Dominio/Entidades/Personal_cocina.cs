90f772a89f70f31680eae308d2962a5af238b2da
KateHurtado <90659145+KateHurtado@users.noreply.github.com>
2021-09-21 18:16:53 -0500

    Update Personal_cocina.cs

--- CTRestaurant.App/CTRestaurant.App.Dominio/Entidades/Personal_cocina.cs
+++ CTRestaurant.App/CTRestaurant.App.Dominio/Entidades/Personal_cocina.cs
@@ -5,7 +5,7 @@ namespace CTRestaurant.App.Dominio
     public class Personal_cocina
     {
         public int Id{get;set;}
-        public int Turno_servcio{get;set;}
+        public int Turno_servcio_cocina{get;set;}
    
 
     }
