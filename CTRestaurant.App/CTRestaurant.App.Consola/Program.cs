cf34fcf989301c818a8d58699e5beeca3acc0617
juanjoseposada <posadajuan691@gmail.com>
2021-09-23 20:01:17 -0500

    Consola

---
+++ CTRestaurant.App/CTRestaurant.App.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CTRestaurant.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
